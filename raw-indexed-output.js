d75f3ecbcad324ffcae1b6c818908728da8a7a84
Florent Biville <fbiville@pivotal.io>
2019-07-18 18:05:12 +0200

    Simplify implementation

--- raw-indexed-output.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = class RawIndexedOutput {
-	constructor(index, contentType, value) {
-		this.index = index;
-		this.contentType = contentType;
-		this.value = value;
-	}
-}
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const {TextEncoder, TextDecoder} = require('util');
 const Stream = require('stream');
 const services = require('./codegen/proto/riff-rpc_grpc_pb');
 const samples = require('./sample-functions');
-const OutputMarshaller = require('./output-marshaller');
+const OutputMarshaller = require('./lib/output-marshaller');
 const messages = require('./codegen/proto/riff-rpc_pb');
 const mediaTypeNegotiator = require('negotiator/lib/mediaType');
-const InputUnmarshaller = require('./input-unmarshaller');
+const RiffFacade = require('./lib/riff-facade');
 const grpc = require('grpc');
 const debug = require('debug')('node-invoker:server');
 
@@ -13,28 +13,20 @@ const debug = require('debug')('node-invoker:server');
 const invoke = (call) => {
 	debug('Invocation started');
 	const userFunction = samples.streamingMultiIoFunction;
-
-	const parameterCount = userFunction.length;
-	debug(`Function accepts ${parameterCount} parameter(s)`);
-	const outputMarshaller = new OutputMarshaller({objectMode: true});
-	const inputUnmarshaller = new InputUnmarshaller(parameterCount, outputMarshaller, {objectMode: true});
-
-	userFunction.apply(null, inputUnmarshaller.parameterStreams);
-	call.pipe(inputUnmarshaller);
-	outputMarshaller.pipe(call);
+	const riffFacade = new RiffFacade(userFunction, call, {objectMode: true});
+	call.pipe(riffFacade);
 
 	call.on('end', () => {
 		debug('Cleaning up before next call');
-		inputUnmarshaller.destroy();
-		outputMarshaller.destroy();
+		riffFacade.destroy();
 	});
 };
 
 const main = () => {
-  const server = new grpc.Server();
-  server.addService(services.RiffService, {invoke: invoke});
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-  server.start();
+	const server = new grpc.Server();
+	server.addService(services.RiffService, {invoke: invoke});
+	server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+	server.start();
 };
 
 main();
