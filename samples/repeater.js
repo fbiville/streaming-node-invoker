5446574c0cc0e878a0ab03acda738c13eae8be6c
Florent Biville <fbiville@pivotal.io>
2019-07-24 14:23:22 +0200

    Remove logger dependency from sample

--- samples/repeater.js
+++ samples/repeater.js
@@ -1,14 +1,12 @@
-const debug = require('debug')('node-invoker:repeater-sample');
-
 module.exports =
 	(numbers /* Readable */, letters /* Readable */, squares /* Writable */, repeated_letters /* Writable */) => {
-		debug('Multi I/O streaming function invocation started');
+		console.log('Multi I/O streaming function invocation started');
 		numbers.on('data', (number) => {
-			debug(`Input #0 received: ${number}`);
+			console.log(`Input #0 received: ${number}`);
 			squares.write(number * number);
 		});
 		letters.on('data', (letter) => {
-			debug(`Input #1 received: ${letter}`);
+			console.log(`Input #1 received: ${letter}`);
 			repeated_letters.write(`${letter}${letter}`);
 		});
 	};
