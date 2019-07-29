668adfd9ef7b5954e8e95b328310cb8ae566de4f
Florent Biville <fbiville@pivotal.io>
2019-07-29 19:20:32 +0200

    Reformat and close output streams when input streams end

--- samples/repeater.js
+++ samples/repeater.js
@@ -1,12 +1,20 @@
 module.exports =
-	(numbers /* Readable */, letters /* Readable */, squares /* Writable */, repeated_letters /* Writable */) => {
-		console.log('Multi I/O streaming function invocation started');
-		numbers.on('data', (number) => {
-			console.log(`Input #0 received: ${number}`);
-			squares.write(number * number);
-		});
-		letters.on('data', (letter) => {
-			console.log(`Input #1 received: ${letter}`);
-			repeated_letters.write(`${letter}${letter}`);
-		});
-	};
+    (numbers /* Readable */, letters /* Readable */, squares /* Writable */, repeated_letters /* Writable */) => {
+        console.log('Multi I/O streaming function invocation started');
+        numbers.on('data', (number) => {
+            console.log(`Input #0 received: ${number}`);
+            squares.write(number * number);
+        });
+        numbers.on('end', () => {
+            console.log(`Input #0 ended`);
+            squares.end();
+        });
+        letters.on('data', (letter) => {
+            console.log(`Input #1 received: ${letter}`);
+            repeated_letters.write(`${letter}${letter}`);
+        });
+        letters.on('end', () => {
+            console.log(`Input #1 ended`);
+            repeated_letters.end();
+        });
+    };
